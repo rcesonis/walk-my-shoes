6ce7c5fee1f82f7de0b5b825e1543b27413d4d76
nextcode1 <stargateblk@gmail.com>
2024-03-26 22:22:30 +0100

    Add authentication routes to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const authApp = require("./routes/auth");
 
 const app = express();
+
+app.use("/", authApp);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
