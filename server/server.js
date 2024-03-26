aa11deecfd62cb35dfe1830d01015cb95f353a98
nextcode1 <stargateblk@gmail.com>
2024-03-26 20:48:45 +0100

    Remove unused code in server.js and add auth file setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-// Use the auth app
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
