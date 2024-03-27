726c53bb156f014015737e88996b73705243bab2
rcesonis <rimvydas.cesonis@gmail.com>
2024-03-27 09:54:07 +0100

    Added custom colors and redux package

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      darkBlue1: "#1C183B",
+      darkBlue2:"232044",
+      darkBlue3: "322E59",
+      customGray: "BCBACF",
+      customYellow: "FFBB56"
+    },
     extend: {},
   },
   plugins: [],
