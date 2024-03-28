6b8a5cd75c8f8f4ffb1026b93c9eebcd43215df2
nextcode1 <stargateblk@gmail.com>
2024-03-28 17:10:42 +0100

    Update Sequelize config file and add urlencoded middleware

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const config = require("./sequelize/config/config");
+const config = require("./sequelize/config/config.json");
 
 const env = process.env.NODE_ENV || "development";
 const dbConfig = config[env];
