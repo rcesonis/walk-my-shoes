8e31254df97fd061ec321380a69681ff6984d96e
nextcode1 <stargateblk@gmail.com>
2024-04-13 13:17:00 +0200

    test

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../../controllers/postController");
+const passport = require("passport");
 
 // Get all posts Protected route
-router.get("/", ensureAuthenticated, postController.getPosts);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  postController.getPosts
+);
 
 // Get posts by ID
-router.get("/:id", ensureAuthenticated, postController.getPostsById);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postController.getPostsById
+);
 
 // Create a new post
-router.post("/", ensureAuthenticated, postController.createPost);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  postController.createPost
+);
 
 // Update an existing post
-router.put("/:id", ensureAuthenticated, postController.updatePost);
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postController.updatePost
+);
 
 // Delete a post
-router.delete("/:id", ensureAuthenticated, postController.deletePost);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postController.deletePost
+);
 
 module.exports = router;
